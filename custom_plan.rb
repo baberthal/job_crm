a4dc9dc8eeadc73ed09274e83090384401fd74e5
Morgan Lieberthal <morgan@jmorgan.org>
2015-08-21 16:08:40 -0600

    zeus config

---
+++ custom_plan.rb
@@ -0,0 +1,14 @@
+require 'zeus/rails'
+
+class CustomPlan < Zeus::Rails
+  def test(*args)
+    require 'simplecov'
+    SimpleCov.start 'rails'
+    ENV['GUARD_RSPEC_RESULTS_FILE'] = 'tmp/guard_rspec_results.txt'
+    Dir["#{Rails.root}/app/**/*.rb"].each { |f| load f }
+    Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+    super
+  end
+end
+
+Zeus.plan = CustomPlan.new
