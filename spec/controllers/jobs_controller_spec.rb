0faac51fd704a97f6063452952dcb537a7dd4b43
Morgan Lieberthal <morgan@jmorgan.org>
2015-08-21 17:35:06 -0600

    spec out show for job

--- spec/controllers/jobs_controller_spec.rb
+++ spec/controllers/jobs_controller_spec.rb
@@ -19,4 +19,19 @@ RSpec.describe JobsController, type: :controller do
       expect(assigns(:jobs).length).to eq 4
     end
   end
+
+  describe 'get #show' do
+    before :each do
+      4.times { create(:job) }
+      get :show, id: 1
+    end
+
+    it 'returns status 200' do
+      expect(response.status).to eq 200
+    end
+
+    it 'renders the show template' do
+      expect(response).to render_template 'show'
+    end
+  end
 end
