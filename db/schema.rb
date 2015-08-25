ec4d9d7659104b7cbf9e7d0c8ee5bee15a32a00b
Morgan Lieberthal <morgan@jmorgan.org>
2015-08-24 21:55:58 -0600

    Migrate and check in schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150821234112) do
+ActiveRecord::Schema.define(version: 20150825032716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "name",                        null: false
+    t.string   "street_address"
+    t.string   "street_address_2"
+    t.string   "city"
+    t.string   "state",            limit: 2
+    t.string   "postal_code",      limit: 12
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+  end
+
+  add_index "companies", ["name"], name: "index_companies_on_name", unique: true, using: :btree
+  add_index "companies", ["street_address", "street_address_2", "city", "state", "postal_code"], name: "by_address", unique: true, using: :btree
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "salutation"
+    t.string   "first_name",   null: false
+    t.string   "last_name",    null: false
+    t.string   "title"
+    t.integer  "company_id"
+    t.string   "main_phone"
+    t.string   "second_phone"
+    t.string   "email"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "contacts", ["company_id"], name: "index_contacts_on_company_id", using: :btree
+  add_index "contacts", ["email"], name: "index_contacts_on_email", using: :btree
+  add_index "contacts", ["first_name", "last_name"], name: "by_full_name", unique: true, using: :btree
+  add_index "contacts", ["main_phone"], name: "index_contacts_on_main_phone", using: :btree
 
   create_table "jobs", force: :cascade do |t|
     t.string   "company_name",     null: false
@@ -22,6 +54,7 @@ ActiveRecord::Schema.define(version: 20150821234112) do
     t.datetime "updated_at",       null: false
     t.datetime "last_touch"
     t.datetime "application_sent"
+    t.string   "position_name"
   end
 
   add_index "jobs", ["company_name"], name: "index_jobs_on_company_name", using: :btree
