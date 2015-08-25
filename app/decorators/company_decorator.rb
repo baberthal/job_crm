ba145d0298f24da6a16c12908e14f0a677f22c40
Morgan Lieberthal <morgan@jmorgan.org>
2015-08-24 23:07:41 -0600

    Add show view for companies and some decorator methods

--- app/decorators/company_decorator.rb
+++ app/decorators/company_decorator.rb
@@ -1,13 +1,17 @@
 class CompanyDecorator < Draper::Decorator
   delegate_all
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
+  def full_address
+    return object.street_address if object.street_address_2.nil?
+    "#{object.street_address}, #{object.street_address_2}"
+  end
 
+  def city_state_zip
+    return unless object.city && object.state && object.postal_code
+    "#{object.city}, #{object.state}, #{object.postal_code}"
+  end
+
+  def phone
+    object.main_phone || object.second_phone || nil
+  end
 end
