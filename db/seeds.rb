06c016e15e95dbcdf28155866cabb081843416d9
Morgan Lieberthal <morgan@jmorgan.org>
2015-08-24 18:12:32 -0600

    Add some fields to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
+Job.destroy_all
+
 10.times do
   Job.create!(company_name: Faker::Company.name,
+              position_name: Faker::Name.title,
               application_sent: 5.minutes.ago,
               last_touch: 4.minutes.ago)
 end
