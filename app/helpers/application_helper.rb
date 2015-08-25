9f08a9c532d1ba9b2f4adc45126b195c9426a311
Morgan Lieberthal <morgan@jmorgan.org>
2015-08-25 00:30:13 -0600

    Add flash messages layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  BOOTSTRAP_CLASSES = {
+    success: 'alert-success',
+    error: 'alert-danger',
+    alert: 'alert-warning',
+    notice: 'alert-info',
+    danger: 'alert-danger'
+  }
+
+  def bootstrap_class_for(flash_type)
+    BOOTSTRAP_CLASSES.fetch(flash_type.to_sym) || flash_type.to_s
+  end
 end
