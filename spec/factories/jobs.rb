b20213585b38163dc17b8449cd1924fcfda45e7e
Morgan Lieberthal <morgan@jmorgan.org>
2015-08-24 18:12:10 -0600

    Fixed FactoryGirl error

--- spec/factories/jobs.rb
+++ spec/factories/jobs.rb
@@ -1,7 +1,6 @@
 FactoryGirl.define do
   factory :job do
     company_name { Faker::Company.name }
-    position_name { Faker::Name.title }
     application_sent { Faker::Date.backward(7) }
     last_touch { application_sent }
   end
