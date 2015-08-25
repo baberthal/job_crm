98a5522cc4ff75bf87eff944f478b002da1b7b42
Morgan Lieberthal <morgan@jmorgan.org>
2015-08-24 21:56:20 -0600

    Create factories for companies and contacts

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -1,6 +1,15 @@
 FactoryGirl.define do
   factory :company do
-    
-  end
+    name { Faker::Company.name }
 
+    trait :with_address do
+      street_address { Faker::Address.street_address }
+      street_address_2 { Faker::Address.secondary_address }
+      city { Faker::Address.city }
+      state { Faker::Address.state_abbr }
+      postal_code { Faker::Address.postcode }
+    end
+
+    factory :company_with_address, traits: [:with_address]
+  end
 end
--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -1,6 +1,34 @@
 FactoryGirl.define do
   factory :contact do
-    
-  end
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
 
+    trait :with_salutation do
+      salutation { Faker::Name.prefix }
+    end
+
+    trait :with_company do
+      company
+    end
+
+    trait :with_phone do
+      main_phone { Faker::PhoneNumber.phone_number }
+    end
+
+    trait :with_all_phones do
+      main_phone { Faker::PhoneNumber.phone_number }
+      second_phone { Faker::PhoneNumber.cell_phone }
+    end
+
+    trait :with_email do
+      email { Faker::Internet.email(first_name) }
+    end
+
+    factory :contact_with_salutation, traits: [:with_salutation]
+    factory :contact_with_company, traits: [:with_company]
+    factory :contact_with_all_info, traits: [:with_salutation,
+                                             :with_company,
+                                             :with_email,
+                                             :with_all_phones]
+  end
 end
