be1fad5f68ecd2acc0acf511038b0865b6363562
Morgan Lieberthal <morgan@jmorgan.org>
2015-08-24 23:59:18 -0600

    Clean up jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,17 +1,9 @@
 class JobsController < ApplicationController
   def index
     @jobs = JobDecorator.decorate_collection(Job.all)
-
-    respond_to do |format|
-      format.html
-    end
   end
 
   def show
     @job = Job.find(params[:id])
-
-    respond_to do |format|
-      format.html
-    end
   end
 end
