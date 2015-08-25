63cce2176f96bc1cc34df691c88622d0cf4c5b49
Morgan Lieberthal <morgan@jmorgan.org>
2015-08-24 21:56:44 -0600

    Preliminary specs for company

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    context 'when only a name is provided' do
+      let(:company) { build(:company) }
+
+      it 'is valid with only a name' do
+        expect(company).to be_valid
+      end
+
+      it 'is invalid without a name' do
+        company.name = nil
+        expect(company).to_not be_valid
+      end
+    end
+
+    context 'when more information is provided' do
+      let(:company) { build(:company_with_address) }
+
+      it 'is valid with all the attributes' do
+        expect(company).to be_valid
+      end
+    end
+  end
 end
