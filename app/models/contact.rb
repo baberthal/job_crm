ccb199b29a40b77b420df8090fdd9368a333dd15
Morgan Lieberthal <morgan@jmorgan.org>
2015-08-24 22:16:37 -0600

    Make the contact specs pass

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,6 @@
 class Contact < ActiveRecord::Base
+  belongs_to :company
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :first_name, uniqueness: [:last_name, :company]
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    shared_examples 'validations' do
+      it 'is valid with valid attributes' do
+        expect(contact).to be_valid
+      end
+
+      it 'is invalid without valid attributes' do
+        contact.first_name = nil
+        expect(contact).to_not be_valid
+      end
+    end
+
+    context 'when only a first and last name are provided' do
+      let(:contact) { build(:contact) }
+      it_behaves_like 'validations'
+    end
+
+    context 'when all attributes are provided' do
+      let(:contact) { build(:contact_with_all_info) }
+      it_behaves_like 'validations'
+    end
+
+    context 'when only a company is provided' do
+      let(:contact) { build(:contact_with_company) }
+      it_behaves_like 'validations'
+    end
+  end
 end
