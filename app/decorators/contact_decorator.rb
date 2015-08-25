5e26cd85198b33cf760509aaf1b4433a38525937
Morgan Lieberthal <morgan@jmorgan.org>
2015-08-24 23:51:11 -0600

    View updates for Company#show and related methods

--- app/decorators/contact_decorator.rb
+++ app/decorators/contact_decorator.rb
@@ -1,13 +1,19 @@
 class ContactDecorator < Draper::Decorator
   delegate_all
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
+  def name
+    "#{object.first_name} #{object.last_name}"
+  end
 
+  def full_name
+    "#{object.salutation} #{object.first_name} #{object.last_name}"
+  end
+
+  def list_group_info
+    html = <<-HTML
+        <span class='text-left'>#{full_name}</span>
+        <span class='pull-right'>#{main_phone if main_phone}</span>
+    HTML
+    html.html_safe
+  end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,5 @@
 class Job < ActiveRecord::Base
   belongs_to :company
   validates :company, presence: true
+  alias_attribute :title, :position_name
 end
